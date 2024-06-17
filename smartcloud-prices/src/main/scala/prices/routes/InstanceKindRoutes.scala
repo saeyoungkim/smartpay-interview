7e57a750cafc686908a429c34978dab4f5056a2a
saeyoung kim <saeyoung@saeyoungnoMacBook-Pro.local>
2024-06-17 23:58:49 +0900

    add memo feature

--- smartcloud-prices/src/main/scala/prices/routes/InstanceKindRoutes.scala
+++ smartcloud-prices/src/main/scala/prices/routes/InstanceKindRoutes.scala
@@ -5,10 +5,12 @@ import cats.effect._
 import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.InvalidMessageBodyFailure
 import org.http4s.server.Router
 import prices.data.InstanceKind
 import prices.routes.protocol._
-import prices.services.InstanceKindService
+import prices.services.{InstanceKindService, SmartcloudInstanceKindService}
+import prices.services.InstanceKindService.Exception.APICallFailure
 
 final case class InstanceKindRoutes[F[_]: Sync](instanceKindService: InstanceKindService[F]) extends Http4sDsl[F] {
 
@@ -20,9 +22,20 @@ final case class InstanceKindRoutes[F[_]: Sync](instanceKindService: InstanceKin
 
   private val get: HttpRoutes[F] = HttpRoutes.of {
     case GET -> Root =>
-      instanceKindService.getAll().flatMap(kinds => Ok(InstanceKindResponse(kinds)))
+      instanceKindService
+        .getAll()
+        .flatMap(kinds => Ok(InstanceKindResponse(kinds)))
     case GET -> Root / kind / "price" =>
-      instanceKindService.get(InstanceKind(kind)).flatMap(price => Ok(InstancePriceResponse(price)))
+      instanceKindService
+        .get(InstanceKind(kind))
+        .flatMap(price => Ok(InstancePriceResponse(price)))
+        .recoverWith {
+          case APICallFailure(message) =>
+            SmartcloudInstanceKindService.getPriceFromMemo(InstanceKind(kind)) match {
+              case Some(price) => Ok(InstancePriceResponse(price))
+              case None => throw APICallFailure(message)
+            }
+        }
   }
 
   def routes: HttpRoutes[F] =
--- smartcloud-prices/src/main/scala/prices/services/SmartcloudInstanceKindService.scala
+++ smartcloud-prices/src/main/scala/prices/services/SmartcloudInstanceKindService.scala
@@ -6,8 +6,11 @@ import io.circe.parser.decode
 import org.http4s.ember.client.EmberClientBuilder
 import prices.data._
 import cats.effect.unsafe.IORuntime
+import org.http4s.InvalidMessageBodyFailure
 import org.http4s.circe.jsonOf
 import prices.data.builder.{InstancePriceRequestBuilder, InstancesRequestBuilder}
+
+import scala.collection.mutable
 
 object SmartcloudInstanceKindService {
 
@@ -15,6 +18,11 @@ object SmartcloudInstanceKindService {
       baseUri: String,
       token: String
   )
+
+  private final val priceMemo = mutable.HashMap[InstanceKind, InstancePrice]()
+
+  def getPriceFromMemo(kind: InstanceKind): Option[InstancePrice] = priceMemo.get(kind)
+  def insertToMemo(kind: InstanceKind, price: InstancePrice): Option[InstancePrice] = priceMemo.put(kind, price)
 
   private final val client = EmberClientBuilder
     .default[IO]
@@ -35,7 +43,7 @@ object SmartcloudInstanceKindService {
           }
           .map(body => decode[List[String]](body) match {
             case Right(v) => v
-            case Left(error) => throw new RuntimeException(error)
+            case Left(error) => throw InvalidMessageBodyFailure(error.getMessage)
           })
           .map(kinds => kinds.map(kind => InstanceKind(kind)))
           .unsafeRunSync()
@@ -46,6 +54,7 @@ object SmartcloudInstanceKindService {
         .use { client =>
           client.expect[InstancePrice](InstancePriceRequestBuilder.build(config, kind))(jsonOf[IO, InstancePrice])
         }
+        .map(price => insertToMemo(kind, price).getOrElse(price))
         .unsafeRunSync()
         .pure[F]
   }
